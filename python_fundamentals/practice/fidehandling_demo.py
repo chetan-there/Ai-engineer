0bdf62e428af8ba25e875ed0ee1a81d319e520f7
chetan-there <therechetan24@gmail.com>
2026-06-11 22:29:14 +0530

    fundamentals

---
+++ python_fundamentals/practice/fidehandling_demo.py
@@ -0,0 +1,21 @@
+file = open("Ai-enginner\python_fundamentals\practice\demo.txt","r") #  r for read 
+
+content = file.read()
+
+print(content)
+
+file = open("Ai-enginner\python_fundamentals\practice\demo.txt", "w") # w mode overwrites old content.
+
+file.write("Hello demo ")
+
+file = open("Ai-enginner\python_fundamentals\practice\demo.txt", "a") # a Old content remains.
+
+file.write("\nLearning Python")
+
+file = open("Ai-enginner\python_fundamentals\practice\demo.txt","r")
+
+content = file.read()
+
+print(content)
+
+file.close
